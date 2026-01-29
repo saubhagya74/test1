19c9edf6a8f1a6929dd4c02e83892d7d5d347dc5
saubhagya74 <saubhagya469@gmail.com>
2026-01-29 21:21:23 +0545

    aalu batching done

--- src/db_workers.rs
+++ src/db_workers.rs
@@ -1,86 +1,163 @@
+use std::collections::HashMap;
+
 use axum::extract::State;
 use axum::http::StatusCode;
-use chrono::DateTime;
+use chrono::{DateTime, Duration};
+use tokio::time::{Instant, timeout};
 
 use crate::{AppState, controllers::message_controller_ws::messagecontroller::MessagePrivateDB};
 use crate::{controllers::message_controller::recent_message_controller::ContentLabel};
 
-
+// pub struct ForBatch{
+//     pub message_id : u64,
+//     pub sender_id : u64,
+//     pub sender_id : u64,
+//     pub sender_id : u64,
+//     pub sender_id : u64,
+// }
+// #[derive(Hash, Eq, PartialEq)]
+// pub struct Hkey{
+//     pub user_a_id: i64,
+//     pub user_b_id: i64
+// }
 pub async fn db_batcher_private(
     state: AppState,
     mut rx: tokio::sync::mpsc::UnboundedReceiver<MessagePrivateDB> 
 ){
-    while let Some(db_rec) = rx.recv().await {
-        //this is for , when there is in memory hashmap of users
-        // let chat_id = if let Some(id) = db_rec.chat_id {
-        //     id as i64 //check the id in memory if stored or not then only returbn 
-        // } else {
-        //     state.bucket_id.lock().await.get_id()
-        // };//this should be in db insread
-        let newchatid=state.bucket_id.lock().await.get_id();
+    // let mut j :i64=1;
+    loop {
+        // println!("batch no:{}",j);
+        let start = Instant::now();
+        let limit = tokio::time::Duration::from_millis(15000);
+        //key is chatid and value is index no in vec
+        // let mut hm=HashMap::<Hkey,u64>::new();
+        let mut i:u64=0;
+        let mut new_chat_ids = Vec::new();
+        let mut message_ids = Vec::new();
+        let mut sender_ids = Vec::new();
+        let mut receiver_ids = Vec::new();
+        let mut user_a_ids = Vec::new();
+        let mut user_b_ids = Vec::new();
+        let mut content_types = Vec::new();
+        let mut descriptions = Vec::new();
+        let mut trimmed_descriptions=Vec::new();
+        let mut messaged_ats = Vec::new();
 
-        let (u_a, u_b) = if db_rec.sender_id < db_rec.receiver_id {
-            (db_rec.sender_id as i64, db_rec.receiver_id as i64)
-        } else {
-            (db_rec.receiver_id as i64, db_rec.sender_id as i64)
-        };
+        while start.elapsed() < limit {
+            let time_remaining = limit.saturating_sub(start.elapsed());
+            println!("time rem:{:?}",time_remaining);
+            match timeout(time_remaining, rx.recv()).await {
+                Ok(Some(db_rec)) => {
 
-        let messaged_at = DateTime::from_timestamp(db_rec.created_at, 0)
-            .unwrap_or_else(|| chrono::Utc::now());
+                    let (u_a, u_b) = if db_rec.sender_id < db_rec.receiver_id {
+                        (db_rec.sender_id as i64, db_rec.receiver_id as i64)
+                    } else {
+                        (db_rec.receiver_id as i64, db_rec.sender_id as i64)
+                    };
+                    //yo time thing pani look more
+                    let messaged_at = DateTime::from_timestamp(db_rec.created_at, 0)
+                        .unwrap_or_else(|| chrono::Utc::now());
 
-        let description_str = std::str::from_utf8(db_rec.description.as_ref())
-            .unwrap_or("");//use if let here
+                    let mut id_gen = state.bucket_id.lock().await;
+                    let msg_id = id_gen.get_id();
+                    let chat_id = id_gen.get_id(); 
+                    drop(id_gen);
+                    // let hkey=Hkey{
+                    //     user_a_id:u_a,
+                    //     user_b_id: u_b
+                    // };
+                    // hm.insert(hkey, i);
 
-        let message_id = state.bucket_id.lock().await.get_id();
-        //check whether the query is correct if y have made a a sperate btree for user ab id
-        //if we know chat id from memory then do normal insert with cached chatid
-        //can use $ for update but used exclued which is better ??
-        //  description_str refers to last_message_?? 
-        // because on no conflict $ 2 is description and its last_message?
-        let res = sqlx::query!(
-            r#"
-            with convo as (
-                insert into conversation_ (chat_id_, last_message_, last_time_, user_a_id_, user_b_id_)
-                values ($1, LEFT($2, 30), $3, $4, $5)
+                    new_chat_ids.push(chat_id);
+                    message_ids.push(msg_id);
+                    sender_ids.push(db_rec.sender_id as i64);
+                    receiver_ids.push(db_rec.receiver_id as i64);
+                    user_a_ids.push(u_a);
+                    user_b_ids.push(u_b);
+                    content_types.push(db_rec.content_type as ContentLabel);
+                    let mut desc=String::from_utf8_lossy(&db_rec.description).into_owned();
+                    // let mut tri_desc=String::new();
+                    // if desc.len()<=31{
+                    //     tri_desc=desc.clone().split_off(30);
+                    // }else{
+                    //     tri_desc=desc.clone();
+                    // } wrong xa ,tala ko gud, but do it in the way tried earlier
+                    // let tri_desc: String = desc.chars().take(30).collect();
+                    let tri_desc = if desc.len() > 30 {
+                        format!("{}...", &desc.chars().take(27).collect::<String>())
+                    } else {
+                        desc.clone()
+                    };
+                    println!("msg:{}in vec",desc);
+                    descriptions.push(desc);
+                    trimmed_descriptions.push(tri_desc);
+                    messaged_ats.push(messaged_at);
+                    i=i+1;
+                },
+                Ok(None) => return, //channel closed, dont do this
+                Err(_) => break,    //500ms passed
+            }
+        }
+
+        // //do limit validation of 2000 in application side
+        // let new_convos=sqlx::query!(
+        //     r#"SELECT 
+        //         r.u_a, 
+        //         r.u_b
+        //     FROM UNNEST($1::int8[], $2::int8[]) AS r(u_a, u_b)
+        //     LEFT JOIN conversation_ c ON c.user_a_id_ = r.u_a AND c.user_b_id_ = r.u_b
+        //     WHERE c.chat_id_ IS NULL
+        //     "#,
+        //     &user_a_ids,
+        //     &user_b_ids
+        // ).fetch_all(&state.db_pool)
+        // .await;
+        // Use the "Mega-Query" logic within sqlx
+        //spqan in task instead
+        let s1=Instant::now();
+        if( !message_ids.is_empty()){
+
+            let result = sqlx::query!(
+                r#"
+            with raw_data as (
+                SELECT * FROM UNNEST(
+                    $1::int8[], $2::int8[], $3::int8[], $4::int8[], 
+                    $5::text[], $6::text[], $7::timestamptz[], $8::int8[], $9::int8[]
+                ) AS t(msg_id, s_id, r_id, new_c_id, descrip , tri_desc, m_at, u_a, u_b)
+            ),
+            upsert_convo as (
+                insert into conversation_ (chat_id_, user_a_id_, user_b_id_, last_message_, last_time_)
+                select distinct on (u_a, u_b) 
+                    new_c_id, u_a, u_b, tri_desc, m_at 
+                from raw_data
                 on conflict (user_a_id_, user_b_id_) 
                 do update set 
-                    last_message_ = excluded.last_message_,
-                    last_time_ = excluded.last_time_
-                returning chat_id_
+                    last_message_ = EXCLUDED.last_message_,
+                    last_time_ = EXCLUDED.last_time_
+                returning chat_id_, user_a_id_, user_b_id_
             )
-            insert into message_ (
-                message_id_, chat_id_, sender_id_, receiver_id_, 
-                content_type_, description_, messaged_at_
-            )
-            select $6, chat_id_, $7, $8, $9 as "content_type_: ContentLabel", $2, $3
-            from convo
+            insert into message_ (message_id_, chat_id_, sender_id_, receiver_id_, description_, messaged_at_)
+            select 
+                r.msg_id, u.chat_id_, r.s_id, r.r_id, r.descrip , r.m_at
+            from raw_data r
+            join upsert_convo u ON r.u_a = u.user_a_id_ AND r.u_b = u.user_b_id_;
             "#,
-            newchatid,
-            description_str,
-            messaged_at,
-            u_a,                
-            u_b,                
-            message_id,
-            db_rec.sender_id as i64,  
-            db_rec.receiver_id as i64,
-            db_rec.content_type as ContentLabel 
-        )
-        .execute(&state.db_pool)
-        .await
-        .map_err(|e| {
-            eprintln!("db failed: {}", e);
-            // StatusCode::INTERNAL_SERVER_ERROR no return its a task
-        });
+            &message_ids,
+            &sender_ids, 
+            &receiver_ids, 
+            &new_chat_ids,
+            &descriptions, 
+            &trimmed_descriptions,
+            &messaged_ats,
+            &user_a_ids, 
+            &user_b_ids
+        ).execute(&state.db_pool).await;
+
+        println!("batch in db in:{}",s1.elapsed().as_millis());
+        }
+        else{
+            println!("no msg to insert");
+        }
+        // j=j+1;
     }
 }
-// pub async fn updateConversation(){
-
-// }
-
-// at first check chatid is sent? 
-// true do 0
-// false do 2
-
-// -0 check for chatid in mem
-// do_> 1 if valid do normal insert and normal update
-// do 2->else do cte common table expresion
